8328ecc8d44c37f692c617b9f663b7829c233662
Christopher Mielack <cmielack@zedat.fu-berlin.de>
2016-03-26 14:52:22 +0100

    Added basic walking behaviour for Hosts.

---
+++ Parasite Survival/Assets/Scripts/HostBehavior.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HostBehavior : MonoBehaviour 
+{
+
+	private ActionMode mode;
+
+	public float walkSpeed = 1.0f;
+
+	private float arrivedTolerance = 0.01f;
+
+	void Start () 
+	{
+		mode = ActionMode.Rest;
+	}
+	
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.Space)) {
+			mode = ActionMode.Walk;
+			GoTo (new Vector3 (-6,0,3));
+		}
+		else{
+			mode = ActionMode.Rest;
+		}
+	}
+
+	void GoTo(Vector3 pos)
+	{
+		Vector3 diff = pos - transform.position;
+
+		Vector3 step = diff.normalized * walkSpeed * Time.deltaTime;
+
+		if (diff.magnitude > step.magnitude) {
+			transform.position += step;
+		} else {
+			transform.position = pos;
+		}
+
+		if ((transform.position - pos).magnitude <= arrivedTolerance) 
+		{
+			Arrive ();
+		}
+	}
+
+	void Arrive()
+	{
+		mode = ActionMode.Rest;
+	}
+
+}
+
+
+public enum ActionMode 
+{
+	Rest,
+	Walk,
+	Shake
+}
---
+++ Parasite Survival/Assets/Scripts/HostManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HostManager : MonoBehaviour 
+{
+
+	public HostBehavior[] hosts;
+
+	void Start () 
+	{
+	
+	}
+
+}
