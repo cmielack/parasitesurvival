163764f1616cdc6fb96c7ee14f2ffb30383e7ad4
Christopher Mielack <cmielack@zedat.fu-berlin.de>
2016-03-26 16:27:59 +0100

    Reworked host behaviour.

--- Parasite Survival/Assets/Scripts/HostBehavior.cs
+++ Parasite Survival/Assets/Scripts/HostBehavior.cs
@@ -4,106 +4,66 @@ using System.Collections.Generic;
 public class HostBehavior : MonoBehaviour 
 {
 
-	private ActionMode mode;
-
 	public float walkSpeed = 1.0f;
-	public float waitTime = 4.0f;
-
-	private float arrivedTolerance = 0.01f;
-
-	private Vector3 currentGoal;
-
-	private Shake currentShake;
 
 	private List<Command> commandQueue;
 
 	void Start () 
 	{
-		mode = ActionMode.Shake;
-		currentGoal = Vector3.zero;
-		currentShake = null;
+		commandQueue = new List<Command> ();
+		
 	}
 	
 	void Update () 
 	{
 		ProcessInput ();
+		if (commandQueue.Count > 0) {
+			var currentCommand = commandQueue [0];
 
-		if (mode == ActionMode.Walk) {
-			GoTo (currentGoal);
-		} 
-		else if (mode == ActionMode.Shake) {
-			DoShake ();
-		}
-		else if (mode == ActionMode.Rest){
-			//...
+			currentCommand.Activate ();
+
+			if (currentCommand.type == "walk") {
+				GoTo (currentCommand);
+				if ((currentCommand.goal - transform.position).magnitude < 0.01f) {
+					commandQueue.RemoveAt (0);
+				}
+			}
+			else if (currentCommand.type == "wait") {
+				// do nothing
+				if (Time.realtimeSinceStartup - currentCommand.startTime > currentCommand.duration) {
+					commandQueue.RemoveAt (0);
+				}
+			}
+
+		} else {
+			Idle ();
 		}
 	}
 
 	void ProcessInput(){
-		if (mode == ActionMode.Rest) {
-			PickRandomGoal ();
-			if(Input.GetKey(KeyCode.Space)){
-				mode = ActionMode.Walk;
-			}
-
-		} else if (mode == ActionMode.Walk) {
-
-		}
 	}
 
-	void GoTo(Vector3 pos)
+	void Idle()
 	{
-		Vector3 diff = pos - transform.position;
+		commandQueue.Add (new Command ("walk", new Vector3 (Random.Range (-8.5f, 8.5f), 0, Random.Range (-4.0f, 4.0f)),
+			walkSpeed, 0));
+		commandQueue.Add (new Command ("wait", Vector3.zero, 0, 1.0f));
+	}
 
-		Vector3 step = diff.normalized * walkSpeed * Time.deltaTime;
+	void GoTo(Command comm)
+	{
+		Vector3 diff = comm.goal - transform.position;
+
+		Vector3 step = diff.normalized * comm.speed * Time.deltaTime;
 
 		if (diff.magnitude > step.magnitude) {
 			transform.position += step;
 		} else {
-			transform.position = pos;
-		}
-
-		if ((transform.position - pos).magnitude <= arrivedTolerance) 
-		{
-			Arrive ();
+			transform.position = comm.goal;
 		}
 	}
 
-	void PickRandomGoal ()
-	{
-		currentGoal = new Vector3 (Random.Range (-8.5f, 8.5f), 0, Random.Range (-4.0f, 4.0f));
-	}
-
-
-	void Arrive()
-	{
-		mode = ActionMode.Rest;
-		Invoke ("SetInMotion", waitTime);
-	}
-
-	void SetInMotion(){
-		mode = ActionMode.Walk;
-		PickRandomGoal ();
-	}
-
-
-	void DoShake()
-	{
-		if (currentShake == null) {
-			// initialise
-			currentShake = new Shake ();
-			currentShake.Randomize ();
-			currentShake.Start (transform.position);
-		} else {
-			currentShake.Update ();
-			transform.position = currentShake.position;
-
-			if (currentShake.IsDone ())
-				currentShake = null;
-		}
-	}
-
-
+		//currentGoal = 
 }
 
 
@@ -115,62 +75,32 @@ public enum ActionMode
 }
 
 
-public class Shake {
-	public Vector3 direction;
-	public float amplitude;
-	public float progress;
-	public float length;
-
-	public float duration;
-	public float timeStarted;
-
-	public Vector3 startPoint;
-	public Vector3 endPoint;
-	public Vector3 position;
-
-	public int countRemaining;
-
-	public void Randomize()
-	{
-		float angle = Random.Range (0, 2.0f * Mathf.PI);
-		direction = new Vector3 (Mathf.Cos (angle), 0, Mathf.Sin (angle));
-		amplitude = 20.0f;
-		progress = 0;
-		length = Random.Range (1.0f, 2.0f);
-		timeStarted = -1.0f;
-		duration = 0.3f;
-		countRemaining = 3;
-	}
-
-	public void Start(Vector3 pos)
-	{
-		timeStarted = Time.realtimeSinceStartup;
-		startPoint = pos - direction * length*0.5f;
-		endPoint   = pos + direction * length*0.5f;
-	}
-
-	public void Update()
-	{
-		progress = (Time.realtimeSinceStartup - timeStarted) / duration;
-		position = startPoint + (endPoint - startPoint) * progress;
-
-		if (progress > 1.0f)
-			countRemaining -= 1;
-	}
-
-	public bool IsDone(){
-		return countRemaining <= 0;
-	}
-
-}
 
 
 public class Command
 {
-	private string type;
-	private float startTime;
-	private float duration;
-	private Vector3 goal;
-	private float speed;
+	public string type;
+	public float startTime;
+	public float duration;
+	public Vector3 goal;
+	public float speed;
 
+	private bool isActivated;	
+
+	public Command(string theType, Vector3 theGoal, float theSpeed, float theDuration)
+	{
+		type = theType;
+
+		isActivated = false;
+		speed = theSpeed;
+		duration = theDuration;
+		goal = theGoal;
+	}
+
+	public void Activate(){
+		if (!isActivated) {
+			startTime = Time.realtimeSinceStartup;
+			isActivated = true;
+		}
+	}
 }
